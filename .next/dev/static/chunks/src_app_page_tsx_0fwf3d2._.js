2af4fa18e8f151c8ec1dcdd38ba1b0aaf5402ce2
plybit <plybit@example.com>
2026-08-02 03:04:23 +0000

    Clean: remove .next cache from tracking, update .gitignore

--- .next/dev/static/chunks/src_app_page_tsx_0fwf3d2._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_0bki3pr._.js","static/chunks/node_modules_1wtsr_p._.js"],
-    source: "dynamic"
-});
