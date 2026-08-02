10ffe3fc74266e7097ef3b35a8779f2e96fb580e
plybit <plybit@example.com>
2026-08-02 02:58:12 +0000

    Remove accidentally committed .env, db, .next, .zscripts (security)

--- .next/dev/server/app/api/candles/[pair]/route.js
+++
@@ -1,7 +0,0 @@
-var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/api/candles/[pair]/route.js")
-R.c("server/chunks/src_app_api_candles_[pair]_route_ts_1i65b9z._.js")
-R.c("server/chunks/[root-of-the-server]__045qe7o._.js")
-R.c("server/chunks/[root-of-the-server]__1ybwdq4._.js")
-R.c("server/chunks/_next-internal_server_app_api_candles_[pair]_route_actions_03cwpdr.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/candles/[pair]/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/candles/[pair]/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
--- .zscripts/run-engine.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-LOG=/home/z/my-project/.zscripts/mini-service-otc-engine.log
-cd /home/z/my-project/mini-services/otc-engine
-while true; do
-  echo "=== [wrapper] starting at $(date -u +%H:%M:%S) ===" >> "$LOG"
-  bun --env-file=/home/z/my-project/.env index.ts >> "$LOG" 2>&1
-  echo "=== [wrapper] exited, restarting in 3s ===" >> "$LOG"
-  sleep 3
-done
