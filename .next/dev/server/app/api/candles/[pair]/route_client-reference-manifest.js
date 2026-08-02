4bfb053ae1ae180d4c4e910e4baf8c32e71d8561
plybit <plybit@example.com>
2026-08-02 02:58:41 +0000

    6305a203-494f-4216-83c0-26b553f02718

---
+++ .next/dev/server/app/api/candles/[pair]/route_client-reference-manifest.js
@@ -0,0 +1,3 @@
+globalThis.__RSC_MANIFEST = globalThis.__RSC_MANIFEST || {};
+globalThis.__RSC_MANIFEST["/api/candles/[pair]/route"] = {"moduleLoading":{"prefix":"","crossOrigin":null},"clientModules":{},"ssrModuleMapping":{},"edgeSSRModuleMapping":{},"rscModuleMapping":{},"edgeRscModuleMapping":{},"entryCSSFiles":{},"entryJSFiles":{}};
+
